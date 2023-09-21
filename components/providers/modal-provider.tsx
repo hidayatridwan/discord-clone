9cd7fc1ba1cc4398c3c4c76b8240a72bf211b8fb
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-09-21 07:05:08 +0700

    modal invite member

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
+import { InviteModal } from "../modals/invite-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -14,7 +15,12 @@ const ModalProvider = () => {
     return null;
   }
 
-  return <CreateServerModal />;
+  return (
+    <>
+      <CreateServerModal />
+      <InviteModal />
+    </>
+  );
 };
 
 export default ModalProvider;
