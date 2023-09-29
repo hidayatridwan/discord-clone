e6134b57dd58b3d8d3c479b1a262c8fbd0d1d143
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-09-29 09:12:55 +0700

    chat components

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -9,7 +11,7 @@ interface ChannelIdPageProps {
   params: {
     serverId: string;
     channelId: string;
-  }
+  };
 }
 
 const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
@@ -18,22 +20,51 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
 
   const channel = await db.channel.findUnique({
     where: {
-      id: params.channelId
-    }
-  })
+      id: params.channelId,
+    },
+  });
 
   const member = await db.member.findFirst({
     where: {
       serverId: params.serverId,
-      profileId: profile.id
-    }
-  })
+      profileId: profile.id,
+    },
+  });
 
-  if (!channel || !member) return redirect("/")
+  if (!channel || !member) return redirect("/");
 
-  return <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-    <ChatHeader name={channel.name} serverId={channel.serverId} type="channel" />
-  </div>;
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
