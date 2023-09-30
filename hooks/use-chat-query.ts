dd868f39a3b66418b555beeb7881106a47e00a88
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2023-09-30 07:05:51 +0700

    chat componenet

---
+++ hooks/use-chat-query.ts
@@ -0,0 +1,19 @@
+import { useSocket } from "@/components/providers/socket-provider";
+import { useParams } from "next/navigation";
+
+interface ChatQueryProps {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+}
+
+export const useChatQuery = ({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) => {
+  const { isConnected } = useSocket();
+  const params = useParams();
+};
